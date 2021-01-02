fd4c446418763482f1036e5ffb122b790d57b679
ferizoozoo <farhadzohoorz@gmail.com>
2021-01-02 07:56:45 +0330

    Add ShortenedUrl model to the shortener app models

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ShortenedUrl(models.Model):
+    url = models.CharField(max_length=200)
+    shortcode = models.CharField(max_length=16, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
