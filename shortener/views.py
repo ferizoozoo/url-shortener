9136c63a4f525a32ca0a3458691a6cf259a80292
ferizoozoo <farhadzohoorz@gmail.com>
2021-01-03 14:12:38 +0330

    Create view for index in shortener app

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+
+from .forms import ShortenUrlForm
 
 # Create your views here.
+class IndexView(FormView):
+    template_name = 'shortener/index.html'
+    form_class = ShortenUrlForm
+    success_url = ''
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
